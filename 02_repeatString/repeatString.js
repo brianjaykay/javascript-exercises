94fef127d48fc9379e8338922a85edbe338172f3
Brian Krause <briankrause.py@gmail.com>
2022-07-17 16:19:56 -0700

    finished exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    combinedString = ""
+    for (let i = 1; i < num; i++;){
+        combinedString += string;
+    }
+    return combinedString
 };
 
 // Do not edit below this line
