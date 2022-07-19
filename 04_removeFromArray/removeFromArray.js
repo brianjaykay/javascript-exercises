eb61254f444ebf4cc1ef9ff1320cb028acae7bc6
Brian Krause <briankrause.py@gmail.com>
2022-07-18 17:22:57 -0700

    Completed exercise 5

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, argus) {
+    const args = Array.from(argus);
+    let newArray = [];
+    if (args.length > 1){
+        for ( let i = 0; i < args.length; i++){
+            console.log(newArray, args);
+            valueToRemove = arr.indexOf(args);
+            newArray = arr.splice(valueToRemove);
+            return newArray;
+            }
+        } else {
+        valueToRemove = arr.indexOf(args);
+        newArray = arr.splice(valueToRemove);
+        return newArray;
+    }r
+    
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,35 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+    let numArray = [];
+    let sumArray = 0;
 
-};
+    if (typeof(numOne) === 'number' && typeof(numTwo) === 'number'){
+        if (numOne < numTwo){
+            numOne = numOne;
+            numTwo = numTwo;
+        } else {
+            let numThree = numTwo;
+            numTwo = numOne;
+            numOne = numThree;
+        } 
+        
+        if (numOne > 0 && numTwo > 0){
+            let end = (numArray.length-1)
+            for (let i = numOne; i < (numTwo + 1); i++){
+                numArray.push(i);
+            }
+            for (const num of numArray){
+                sumArray += num;
+                
+            } 
+            return sumArray;
+        }else {
+            return "ERROR";
+        }  
+    } else {
+        return "ERROR";
+    }
+    }
+
 
 // Do not edit below this line
 module.exports = sumAll;
