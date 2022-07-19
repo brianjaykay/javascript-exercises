6b614efe08dbde1825136bb7f0f3bf717e1f1b8b
Brian Krause <briankrause.py@gmail.com>
2022-07-18 19:35:43 -0700

    Finished exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,52 @@
-const add = function() {
-	
+const add = function(numOne, numTwo) {
+  return numOne + numTwo;
 };
 
-const subtract = function() {
-	
+const subtract = function(numOne, numTwo) {
+	return numOne - numTwo;
 };
 
-const sum = function() {
-	
+const sum = function(args) {
+  let sum = 0;
+  const argLength = (args.length)
+    for (let i=0; i < argLength; i++){
+      sum += args[i];
+    }
+    return sum;
+  };
+
+const multiply = function(args) {
+  let sum = 1;
+  const argLength = (args.length)
+    for (let i=0; i < argLength; i++){
+      sum *= args[i];
+    }
+    return sum;
 };
 
-const multiply = function() {
-
+const power = function(numOne, numTwo) {
+   return numOne ** numTwo;
 };
 
-const power = function() {
-	
-};
+const factorial = function(num) {
+  let numArray = [];
+  
+  for (let i = 1; i < (num); i++){
+    numArray.push(i);
+  }
+  numArray.push(num);
 
-const factorial = function() {
-	
+  const numLength = (numArray.length);
+
+  if (numLength > 1){
+    let start = numArray[0]
+    for (let i=1; i < numLength; i++){
+        start *= numArray[i];
+      }
+      return start;
+    } else {
+      return 1;
+    }
 };
 
 // Do not edit below this line
