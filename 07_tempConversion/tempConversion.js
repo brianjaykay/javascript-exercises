553703909fe4e45fcd0df45bdf2b389151313b2c
Brian Krause <briankrause.py@gmail.com>
2022-07-18 17:51:31 -0700

    Completed tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(deg) {
+  let f = deg;
+  let c = (f-32)* 5/9;
+  return parseFloat(c.toFixed(1))
 };
 
-const ctof = function() {
-
+const ctof = function(deg) {
+  let c = deg;
+  let f = (c * 9/5) +32;
+  return parseFloat(f.toFixed(1))
 };
 
 // Do not edit below this line
